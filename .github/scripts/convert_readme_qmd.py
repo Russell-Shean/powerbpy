6ce10b5d78e39dabaeba35251b3f2a84a5dbe28a
Russell Shean <119683040+DOH-RPS1303@users.noreply.github.com>
2025-02-13 10:29:24 -0800

    simplify readme modifying parts of github actions

--- .github/scripts/convert_readme_qmd.py
+++ .github/scripts/convert_readme_qmd.py
@@ -2,8 +2,10 @@
 This file switches out the code highlighting blocks because Github and pandoc use different code highlighting services. 
 Github recognizes batch scripts. Pandoc doesn't so we're going to call dosbat close enough for the quarto site.
 
-We're also going to try adding a qmd title, although I doubt that will work and I'll probably need to mess with the html file instead. 
-This script will run on github actions each time the quartodoc rendering + gh page publishing action runs.
+We're also going to append some Javascript to: 
+	1. Change the page title 
+	2. Resize the smaller image and change its css float property to none 
+	3. Remove quarto figure captions
 '''
 
 og_readme_path = "README.md"
@@ -18,7 +20,7 @@ with open(index_qmd_path, "w") as new_file:
 	new_file.write("include-after-body:\n")
 	new_file.write(" text: |\n")
 
-	# super duper dumb js~ Yay~~ 
+	#  js~ Yay~~ 
 	# change document title
 	new_file.write('  <script>document.title = "Power Bpy";')
 
@@ -26,7 +28,7 @@ with open(index_qmd_path, "w") as new_file:
 	new_file.write('  document.querySelector("a.nav-link:nth-child(2) > img:nth-child(1)").style.height="140px";')
 	new_file.write('  document.querySelector("a.nav-link:nth-child(2) > img:nth-child(1)").style.float="none";')
 
-	# remove the stupid figure captions on images
+	# remove the figure captions on images
 	# I could find the quarto setting, but this is easier lol
 	# but I should eventually make this a standalone script....
 
@@ -34,14 +36,17 @@ with open(index_qmd_path, "w") as new_file:
 	new_file.write('  const fig_captions = document.querySelectorAll(".figure > figcaption");')
 
 	# loop through them and remove them
-	new_file.write('  for(let i = 0; i < fig_captions.length; i++){fig_captions[i].remove();}')
+	new_file.write('  for(let caption of fig_captions){caption.remove();}')
 
 	new_file.write('  </script>\n')
 	new_file.write("---\n\n")
 
-
+	# write the old file to the new file
+	# after all the title stuff above
 	with open(og_readme_path, "r") as old_file:
 		for line in old_file.readlines():
+
+			# Replace the code highlighting language
 			line = line.replace("batchfile", "dosbat")
 			new_file.write(line)
 
