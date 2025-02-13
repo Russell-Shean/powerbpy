bb7228ffbf45d1b22386ec31ac6ffea8fd629654
Russell Shean <119683040+DOH-RPS1303@users.noreply.github.com>
2025-02-12 16:15:33 -0800

    rework worked_examples

--- .github/scripts/convert_ref_index_qmd.py
+++ .github/scripts/convert_ref_index_qmd.py
@@ -8,19 +8,20 @@ This script will run on github actions each time the quartodoc rendering + gh pa
 
 import shutil
 
-og_path = "docs/reference/index.qmd"
-temp_path = "this_is_stupid.qmd"
 
+def add_html_title(og_path, title):
 
-with open(temp_path, "w") as new_file:
-	new_file.write("---\n")
-	new_file.write('Title: "Power Bpy - Function Reference"\n')
+	temp_path = "this_is_stupid.qmd"
 
-	# include a js script to update the title
-	new_file.write("include-before-body:\n")
-	new_file.write(" text: |\n")
-	new_file.write('  <script>document.title = "Power Bpy - Function Reference";</script>\n')
-	new_file.write("---\n\n")
+	with open(temp_path, "w") as new_file:
+		new_file.write("---\n")
+		new_file.write('Title: "Power Bpy - Function Reference"\n')
+
+		# include a js script to update the title
+		new_file.write("include-before-body:\n")
+		new_file.write(" text: |\n")
+		new_file.write(f'  <script>document.title = "{title}";</script>\n')
+		new_file.write("---\n\n")
 
 
 	with open(og_path, "r") as old_file:
@@ -28,6 +29,14 @@ with open(temp_path, "w") as new_file:
 			new_file.write(line)
 
 
-shutil.move(temp_path, og_path)
+	shutil.move(temp_path, og_path)
 
+# Change titles
+og_path = "docs/reference/index.qmd"
+title = "Power Bpy - Function Reference"
+add_html_title(og_path, title)
+
+og_path = "docs/worked_examples/index.qmd"
+title = "Power Bpy - Worked examples"
+add_html_title(og_path, title)
 
