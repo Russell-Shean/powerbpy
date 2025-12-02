f20385b0d0536d99b0fc706258efba18f27cf8cf
Russell-Shean <russshean@gmail.com>
2025-12-02 11:25:23 +0800

    additional changes to oop

--- src/oop_test.py
+++ src/oop_test.py
@@ -5,7 +5,11 @@ import os
 
 my_dashboard = Dashboard(os.getcwd(), "test_dashboard")
 
-my_dashboard.new_page(page_name = "Bigfoot Map",
- title= "Bigfoot sightings", 
- subtitle = "By Washington Counties")
+page2 = my_dashboard.new_page(page_name = "Bigfoot Map",
+                              title= "Bigfoot sightings", 
+                              subtitle = "By Washington Counties")
+
+page2.add_background_image(img_path = "/home/russ/Documents/coding_projects/portfolio_projects/powerbpy/examples/data/Taipei_skyline_at_sunset_20150607.jpg", 
+	               alpha = 51,
+	               scaling_method = "Fit")
 
---
+++ src/powerbpy/chart.py
@@ -0,0 +1,4 @@
+import  os, json, re
+
+from powerbpy.dashboard import Dashboard
+from powerbpy.page import Page
