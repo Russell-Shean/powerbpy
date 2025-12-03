c6ff5958ea19380861b6e2f454e7dc8e5bef6020
Russell-Shean <russshean@gmail.com>
2025-12-03 19:24:52 +0800

    start adding chart

--- src/oop_test.py
+++ src/oop_test.py
@@ -16,3 +16,19 @@ page2.add_background_image(img_path = "C:/Users/rshea/coding_projects/powerbpy/e
 	               alpha = 51,
 	               scaling_method = "Fit")
 
+page2.add_chart( 
+	      page_id = "page2", 
+	      chart_id = "colonies_lost_by_year", 
+	      chart_type = "columnChart",
+	      data_source = "colony",
+	      chart_title = "Number of Bee Colonies Lost per Year",
+	      x_axis_title = "Year",
+	      y_axis_title = "Number of Colonies",
+	      x_axis_var = "year",
+	      y_axis_var = "colony_lost",
+	      y_axis_var_aggregation_type = "Sum",
+	      x_position = 23,
+	      y_position = 158,
+	      height = 524,
+	      width = 603)
+
