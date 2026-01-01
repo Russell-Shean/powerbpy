b236974fd65d6490403ad5656b9e73161777b579
Russell-Shean <russshean@gmail.com>
2026-01-01 13:58:26 +0800

    add argument

--- src/powerbpy/shape.py
+++ src/powerbpy/shape.py
@@ -4,7 +4,7 @@ import json
 
 from powerbpy.visual import _Visual
 
-class _SankyChart(_Visual):
+class _Shape(_Visual):
     """This class is for shapes such as arrows that you can add to a page"""
 
     # pylint: disable=too-few-public-methods
@@ -28,25 +28,27 @@ class _SankyChart(_Visual):
                             z_position,
                             alt_text="A shape"):
 
-        '''This function adds a new chart to a page in a power BI dashboard report.
+        '''This function adds a new shape to a page in a power BI dashboard report.
 
         Parameters
         ----------
-        alt_text: str
+        shape_type : str      
+            The type of shape you want to put on the page. For example an arrow would be "arrow"
+        alt_text : str
             Alternate text for the visualization can be provided as an argument. This is important for screen readers (accesibility) or if the visualization doesn't load properly.
         chart_title_font_size: int
             Font size for chart title
-        x_position: int
+        x_position : int
             The x coordinate of where you want to put the chart on the page. Origin is page's top left corner.
-        y_position: int
+        y_position : int
             The y coordinate of where you want to put the chart on the page. Origin is page's top left corner.
-        height: int
+        height : int
             Height of chart on the page
-        width: int
+        width : int
             Width of chart on the page
-        tab_order: int
+        tab_order : int
             The order which the screen reader reads different elements on the page. Defaults to -1001 for now. (I need to do more to figure out what the numbers correpond to. It should also be possible to create a function to automatically order this left to right top to bottom by looping through all the visuals on a page and comparing their x and y positions)
-        z_position: int
+        z_position : int
             The z index for the visual. (Larger number means more to the front, smaller number means more to the back). Defaults to 6000
 
         '''
